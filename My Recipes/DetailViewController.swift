4d4ce9c74cd29e0ca24205965d1382693f65a331
Jorge Eduardo <jeduardo1601@gmail.com>
2017-08-14 16:53:49 -0500

    Working with sections on table view to add ingredients and steps for each recipe - Fixing UI to show labels properly

--- My Recipes/DetailViewController.swift
+++ My Recipes/DetailViewController.swift
@@ -40,3 +40,94 @@ class DetailViewController: UIViewController {
     */
 
 }
+
+extension DetailViewController : UITableViewDataSource {
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 3
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        switch section {
+        case 0:
+            return 3
+        case 1:
+            return self.recipe.ingredients.count
+        case 2:
+            return self.recipe.steps.count
+        default:
+            return 0
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "itemDetailCell", for: indexPath) as! RecipeDetailItemCell
+        
+        switch indexPath.section {
+        case 0:
+            switch indexPath.row {
+            case 0:
+                cell.keyLabel.text = "Nombre: "
+                cell.valueLabel.text = recipe.name
+            case 1:
+                cell.keyLabel.text = "Tiempo: "
+                cell.valueLabel.text = "\(recipe.time!) mins"
+            case 2:
+                cell.keyLabel.text = "Favorito"
+                var isFavoriteText : String = "No"
+                if self.recipe.isFavorite{
+                    isFavoriteText = "Si"
+                }
+                cell.valueLabel.text = isFavoriteText
+            default:
+                break
+            }
+        case 1:
+            cell.keyLabel.text = ""
+            
+            if indexPath.row == 0{
+                cell.keyLabel.text = "Ingredientes: "
+            }
+            
+            cell.valueLabel.text = self.recipe.ingredients[indexPath.row]
+            
+        case 2:
+            cell.keyLabel.text = ""
+            
+            if indexPath.row == 0{
+                cell.keyLabel.text = "Pasos: "
+            }
+            
+            cell.valueLabel.text = self.recipe.steps[indexPath.row]
+            
+        default:
+            break;
+        }
+        
+        return cell
+        
+    }
+    
+    
+    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        var title = ""
+        
+        switch section {
+        case 0:
+            break
+        case 1:
+            title = "Ingredientes"
+        case 2:
+            title = "Pasos"
+        default:
+            break
+        }
+        return title
+    }
+    
+}
+
+extension DetailViewController : UITableViewDelegate {
+    
+}
